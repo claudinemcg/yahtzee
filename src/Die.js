446b030e53eefe409ec58a7091cf6459a1bfd637
Claudine McGowan <claudine.mcgowan@gmail.com>
2022-01-14 14:21:30 +0000

    change arrow function in Die.js to handleClick()

--- src/Die.js
+++ src/Die.js
@@ -2,14 +2,23 @@ import React, { Component } from "react";
 import "./Die.css";
 
 class Die extends Component {
+  constructor(props){
+    super(props)
+    this.handleClick = this.handleClick.bind(this);
+  }
+  
+  handleClick(){
+    this.props.handleClick(this.props.idx)
+    // calling from Game to Dice to Die
+      // pass in this.props.idx so toggleLocked(idx) can use the idx
+  }
   render() {
     return (
       <button
         className={"Die"}
         style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
-        onClick={() => this.props.handleClick(this.props.idx)} 
-        // pass in this.props.idx so toggleLocked(idx) can use the idx
-      >
+        onClick={this.handleClick} 
+      > 
         {this.props.val}
       </button>
     );
