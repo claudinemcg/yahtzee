2b8325e6f3c9974944696745f15c27b551a9f9ca
Claudine McGowan <claudine.mcgowan@gmail.com>
2022-01-18 14:27:41 +0000

    fixed bug so user can't override scores

--- src/RuleRow.js
+++ src/RuleRow.js
@@ -4,7 +4,9 @@ import './RuleRow.css'
 class RuleRow extends Component {
   render() {
     return (
-      <tr className="RuleRow RuleRow-active" onClick={this.props.doScore}>
+      <tr className={`RuleRow RuleRow-${this.props.score === undefined ? 'active': 'disabled'}`}
+          onClick={this.props.score === undefined ? this.props.doScore : null}>  
+          {/* if there's no score then, give it a score when clicked otherwise you can't add in a new score */}
         <td className="RuleRow-name">{this.props.name}</td>
         <td className="RuleRow-score">{this.props.score}</td>
       </tr>
